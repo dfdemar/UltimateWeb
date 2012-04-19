619cedb5e0b9112c5f8275249730fa26ca8cc440
Jim Geppert <jimgeppert@gmail.com>
2012-04-18 19:52:01 -0500

    x

---
+++ UltimateWeb/src-test/com/summithill/ultimate/controller/ParameterGameTest.java
@@ -0,0 +1,21 @@
+package com.summithill.ultimate.controller;
+
+import static org.junit.Assert.*;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParameterGameTest {
+	
+	@Before
+	public void setup() {
+	}
+	
+	@Test
+	public void testUnMarshalling() throws Exception {
+		String json = "{\"tournamentName\":\"trouble at vegas\",\"opponentName\":\"foos\",\"ours\":1103100,\"gameId\":\"game-613E90F4-1CDC-48C2-8E1B-CDE8D346D0E3\",\"gamePoint\":13,\"timestamp\":\"2012-04-15 20:53\",\"isFirstPointOline\":false,\"wind\":{\"mph\":0,\"leftToRight\":true,\"degrees\":-1},\"teamId\":\"4936\",\"theirs\":534643}";
+		ParameterGame unmarshalledGame = new ObjectMapper().readValue(json, ParameterGame.class);
+		assertNotNull(unmarshalledGame);
+	}
+}
