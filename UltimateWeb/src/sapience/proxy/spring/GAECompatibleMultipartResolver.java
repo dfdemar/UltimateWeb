5b2703e8686a09067a50eafd1ab494ff2305c904
Jim Geppert <jimgeppert@gmail.com>
2013-05-27 11:23:49 -0500

    switched file upload handler to one that will work in google app engine

---
+++ UltimateWeb/src/sapience/proxy/spring/GAECompatibleMultipartResolver.java
@@ -0,0 +1,106 @@
+/*
+* Copyright 2002-2006 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+ 
+
+package sapience.proxy.spring;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
+
+
+/**
+ * Inspired by: http://github.com/pjesi/springextras/tree/master/src/main/java/is/hax/spring/web/multipart/
+ * and http://code.google.com/appengine/kb/java.html#fileforms 
+ * @author pajoma
+ *
+ */
+public class GAECompatibleMultipartResolver extends CommonsMultipartResolver {
+
+	 public MultipartHttpServletRequest resolveMultipart(HttpServletRequest request) throws MultipartException {
+		 
+	        // Create a new file upload handler
+	        ServletFileUpload upload = new ServletFileUpload();
+	        upload.setFileSizeMax(getFileUpload().getFileSizeMax());
+	 
+	        String encoding = determineEncoding(request);
+	 
+	        //Map<String, MultipartFile> multipartFiles = new HashMap<String, MultipartFile>();
+	        Map<String, String[]> multipartParameters = new HashMap<String, String[]>();
+	 
+	 
+	        MultiValueMap<String, MultipartFile> multipartFiles = new LinkedMultiValueMap<String, MultipartFile>();
+	 
+	 
+	 
+	        // Parse the request
+	        try {
+	            FileItemIterator iter = upload.getItemIterator(request);
+	            while (iter.hasNext()) {
+	                FileItemStream item = iter.next();
+	 
+	 
+	                String name = item.getFieldName();
+	                InputStream stream = item.openStream();
+	                if (item.isFormField()) {
+	 
+	                    String value = Streams.asString(stream, encoding);
+	 
+	                    String[] curParam = (String[]) multipartParameters.get(name);
+	                    if (curParam == null) {
+	                        // simple form field
+	                        multipartParameters.put(name, new String[]{value});
+	                    } else {
+	                        // array of simple form fields
+	                        String[] newParam = StringUtils.addStringToArray(curParam, value);
+	                        multipartParameters.put(name, newParam);
+	                    }
+	 
+	 
+	                } else { 
+	 
+	                    // Process the input stream
+	                    MultipartFile file = new StreamingMultipartFile(item);
+	                    multipartFiles.add(name, file);
+	                }
+	            }
+	        } catch (IOException e) {
+	            throw new MultipartException("something went wrong here",e);
+	        } catch (FileUploadException e) {
+	            throw new MultipartException("something went wrong here",e);
+	        }
+	 
+	 
+	        return new DefaultMultipartHttpServletRequest(request, multipartFiles, multipartParameters, null);
+	    }
+}
