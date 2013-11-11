1d7e067fc0c747c146fca975d0431a356cd1f8dd
Kyle Geppert <krgeppert@gmail.com>
2013-11-11 12:11:58 -0800

    Minimized project folders

--- UltimateWeb/war/beta/app/scripts/controllers/downloadPage.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('iUtltimateApp')
-  .controller('DownloadpageCtrl', function($scope, Rest) {
-    $scope.downloadUrl = Rest.urlForStatsExportFileDownload($scope.teamId, null);
-  });
