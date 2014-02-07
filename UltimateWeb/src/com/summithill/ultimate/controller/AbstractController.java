51a5b64243968c6408cc337152f71ccf3184c0a2
Jim Geppert <jimgeppert@gmail.com>
2014-02-07 09:58:01 -0600

    Fix team password issue caused by previous check-in.  We now have 401 scenarios: mobile updates (which include a realm header) and password challenges (which do not).  When the realm was present on the password challenge the browser intercepted the 401 and asked the user for credentials - part II

--- UltimateWeb/src/com/summithill/ultimate/controller/AbstractController.java
+++ UltimateWeb/src/com/summithill/ultimate/controller/AbstractController.java
@@ -172,6 +172,8 @@ public class AbstractController {
 	protected void logErrorAndThrow(String message, Throwable t) {
 		if (t instanceof UnauthorizedException) {
 			throw (UnauthorizedException)t;
+		} else if (t instanceof PasswordRequiredException) {
+			throw (PasswordRequiredException)t;
 		}
 		if (t == null) {
 			log.log(SEVERE, message);
