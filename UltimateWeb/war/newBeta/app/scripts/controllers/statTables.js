e1abc9f734dfaa0fa534214c715a761d43800c90
Kyle Geppert <krgeppert@gmail.com>
2014-03-15 23:05:30 -0700

    Tad bit of code cleanup.

--- UltimateWeb/war/newBeta/app/scripts/controllers/statTables.js
+++ UltimateWeb/war/newBeta/app/scripts/controllers/statTables.js
@@ -9,39 +9,43 @@ angular.module('newBetaApp')
       $scope.focus = type;
     };
     playerStats.then(function(statsApi) {
-      $scope.categories = [{
-        name: 'Passing',
-        statTypes: ['assists', 'completions', 'throwaways','stalls', 'passingPercentage']
-      }, {
-        name: 'Receiving',
-        statTypes: ['goals','catches','touches','drops','catchingPercentage']
-      }, {
-        name: 'Playing Time',
-        statTypes: ['gamesPlayed','pointsPlayed','timePlayedMinutes', 'oPoints', 'dPoints']
-      }, {
-        name: 'Defense',
-        statTypes: ['ds','pulls','callahans','averagePullHangtime','oBPulls',]
-      }, {
-        name: 'Per Point',
-        statTypes: ['ppGoals' ,'ppAssists', 'ppDs' , 'ppThrowaways' , 'ppDrops']
-      }, ];
-      $scope.focus = $scope.categories[0];
-      $scope.games = filter.included;
-      $scope.stats = statsApi.getFrom(filter.included);
+      playerStats = statsApi;
+      init();
+    });
+    $scope.categories = [{
+      name: 'Passing',
+      statTypes: ['assists', 'completions', 'throwaways','stalls', 'passingPercentage']
+    }, {
+      name: 'Receiving',
+      statTypes: ['goals','catches','touches','drops','catchingPercentage']
+    }, {
+      name: 'Playing Time',
+      statTypes: ['gamesPlayed','pointsPlayed','timePlayedMinutes', 'oPoints', 'dPoints']
+    }, {
+      name: 'Defense',
+      statTypes: ['ds','pulls','callahans','averagePullHangtime','oBPulls',]
+    }, {
+      name: 'Per Point',
+      statTypes: ['ppGoals' ,'ppAssists', 'ppDs' , 'ppThrowaways' , 'ppDrops']
+    }, ];
+    $scope.focus = $scope.categories[0];
+    $scope.games = filter.included; // updated by the filter controller.
+    function init(){
+      $scope.stats = playerStats.getFrom(filter.included);
       $scope.players = Object.keys($scope.stats);
       $scope.statTypes = Object.keys($scope.stats[$scope.players[0]].stats);
-      $scope.$watchCollection('games', function() {
-        $scope.stats = statsApi.getFrom(filter.included);
-        $scope.statsArray = _($scope.stats).toArray();
-        $scope.players = Object.keys($scope.stats);
-        $scope.statTypes = Object.keys($scope.stats[$scope.players[0]].stats);
-      });
-      $scope.sorter = '-name';
-      $scope.sort = function(obj, prop){
-        var name;
-        prop ? name = obj + '.' + prop : name = obj;
-        ($scope.sorter === name) ? $scope.sorter = '-' + $scope.sorter : $scope.sorter = name;
-      };
+    }
+    $scope.$watchCollection('games', function() {
+      $scope.stats = playerStats.getFrom(filter.included);
+      $scope.statsArray = _($scope.stats).toArray();
+      $scope.players = Object.keys($scope.stats);
+      $scope.statTypes = Object.keys($scope.stats[$scope.players[0]].stats);
     });
+    $scope.sorter = '-name';
+    $scope.sort = function(obj, prop){
+      var name;
+      prop ? name = obj + '.' + prop : name = obj;
+      ($scope.sorter === name) ? $scope.sorter = '-' + $scope.sorter : $scope.sorter = name;
+    };
   });
   
