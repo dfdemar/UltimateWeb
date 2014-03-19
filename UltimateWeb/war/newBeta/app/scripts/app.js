627c4da9ed0127008217fc9955bb5d4660a114cf
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-03-18 20:52:33 -0700

    Back button is back.

--- UltimateWeb/war/newBeta/app/scripts/app.js
+++ UltimateWeb/war/newBeta/app/scripts/app.js
@@ -11,17 +11,14 @@ angular.module('newBetaApp', [
         next.set($location.path());
         return Authorization.ping($location.url().match(/^\/\d+/)[0].slice(1));
       }
-      function purgeSearchParam ($location){
-        $location.search('');
-      }
       $routeProvider
         .when('/', {templateUrl: 'views/splash.html', controller: 'SplashCtrl'}) .when('/:teamId/login', {templateUrl: 'views/login.html', controller: 'LoginCtrl'})
-        .when('/:teamId/players', {templateUrl: 'views/players.html', controller: 'PlayersCtrl', resolve: {authorized: checkAuth, purge:purgeSearchParam}})
-        .when('/:teamId/team', {templateUrl: 'views/team.html', controller: 'TeamCtrl', resolve: {authorized: checkAuth, purge:purgeSearchParam}})
-        .when('/:teamId/download', {templateUrl: 'views/download.html', controller: 'DownloadCtrl', resolve: {authorized: checkAuth, purge:purgeSearchParam}})
-        .when('/:teamId/line', {templateUrl: 'views/line.html', controller: 'LineCtrl', resolve: {authorized: checkAuth, purge:purgeSearchParam}})
-        .when('/:teamId/games', {templateUrl: 'views/games.html', controller: 'GamesCtrl', resolve: {authorized: checkAuth}, reloadOnSearch: false})
-        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth, purge:purgeSearchParam}})
+        .when('/:teamId/players', {templateUrl: 'views/players.html', controller: 'PlayersCtrl', resolve: {authorized: checkAuth}})
+        .when('/:teamId/team', {templateUrl: 'views/team.html', controller: 'TeamCtrl', resolve: {authorized: checkAuth}})
+        .when('/:teamId/download', {templateUrl: 'views/download.html', controller: 'DownloadCtrl', resolve: {authorized: checkAuth}})
+        .when('/:teamId/line', {templateUrl: 'views/line.html', controller: 'LineCtrl', resolve: {authorized: checkAuth}})
+        .when('/:teamId/games', {templateUrl: 'views/games.html', controller: 'GamesCtrl', resolve: {authorized: checkAuth}})
+        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth}})
         .when('/404', {templateUrl: 'views/404.html', controller: '404Ctrl'})
         .otherwise({redirectTo: '/404'});
     }
