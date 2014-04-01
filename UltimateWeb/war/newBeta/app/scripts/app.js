34f202b2951bd35ab19fc34d9bc9e42ab59e731b
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-03-31 20:11:41 -0700

    Removed the playername validation, it's breaking things and not provinding any value.

--- UltimateWeb/war/newBeta/app/scripts/app.js
+++ UltimateWeb/war/newBeta/app/scripts/app.js
@@ -11,18 +11,18 @@ angular.module('newBetaApp', [
         next.set($location.path());
         return Authorization.ping($location.url().match(/^\/\d+/)[0].slice(1));
       }
-      function checkPlayerExistence ($q, $location, api) {
-        api.retrieveTeam($location.url().match(/^\/\d+/)[0].slice(1), true, function(response){
-          if (_(response.players).pluck('name').contains(decodeURI($location.url().match(/\/[^\/]+$/)[0].slice(1)))){
-            deferred.resolve();
-          } else {
-            $location.url('/404');
-            deferred.resolve(new Error('bad player name'));
-          }
-        });
-        var deferred = $q.defer();
-        return deferred.promise;
-      }
+      // function checkPlayerExistence ($q, $location, api) {
+      //   api.retrieveTeam($location.url().match(/^\/\d+/)[0].slice(1), true, function(response){
+      //     if (_(response.players).pluck('name').contains(decodeURI($location.url().match(/\/[^\/]+$/)[0].slice(1)))){
+      //       deferred.resolve();
+      //     } else {
+      //       $location.url('/404');
+      //       deferred.resolve(new Error('bad player name'));
+      //     }
+      //   });
+      //   var deferred = $q.defer();
+      //   return deferred.promise;
+      // }
 
 
       $routeProvider
@@ -32,7 +32,7 @@ angular.module('newBetaApp', [
         .when('/:teamId/download', {templateUrl: 'views/download.html', controller: 'DownloadCtrl', resolve: {authorized: checkAuth}})
         .when('/:teamId/line', {templateUrl: 'views/line.html', controller: 'LineCtrl', resolve: {authorized: checkAuth}})
         .when('/:teamId/games', {templateUrl: 'views/games.html', controller: 'GamesCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth, playerExists: checkPlayerExistence}})
+        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth}})
         .when('/404', {templateUrl: 'views/404.html', controller: '404Ctrl'})
         .otherwise({redirectTo: '/404'});
     }
